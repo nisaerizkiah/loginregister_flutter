13266b870570cb106ccfa450539d6d881a431a35
nisaerizkiah <nisaerizkiah@gmail.com>
2025-09-15 13:57:42 +0700

    push

---
+++ lib/data/user_data.dart
@@ -0,0 +1,9 @@
+/// Variabel global untuk menyimpan data pengguna.
+/// Struktur: Map<String, Map<String, String>>
+/// Key luar = email, value = { fullName, password }
+Map<String, Map<String, String>> userData = {
+  'test@email.com': {
+    'fullName': 'Test User',
+    'password': 'password123',
+  },
+};
